45e205738985064d005662742a52d65028606e8a
rwirba <rwirba@protonmail.com>
2025-07-04 23:41:51 -0500

    test

--- src/test/java/com/ezlearn/services/CourseServiceTest.java
+++ src/test/java/com/ezlearn/services/CourseServiceTest.java
@@ -2,19 +2,23 @@ package com.ezlearn.services;
 
 import com.ezlearn.models.Course;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
+/**
+ * Tests for CourseService.
+ */
 class CourseServiceTest {
+
     @Test
-    void getAllCourses_ReturnsCourses() {
+    void testGetAllCoursesReturnsCourses() {
         CourseService service = new CourseService();
-        assertEquals(2, service.getAllCourses().size());
+        Assertions.assertEquals(2, service.getAllCourses().size());
     }
 
     @Test
-    void getCourseById_ReturnsCourse() {
+    void testGetCourseByIdReturnsCourse() {
         CourseService service = new CourseService();
         Course course = service.getCourseById("TEST-123");
-        assertEquals("TEST-123", course.getId());
+        Assertions.assertEquals("TEST-123", course.getId());
     }
 }
