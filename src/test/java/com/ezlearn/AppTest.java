c7fdc0906c34206d3c18bbe570e57806f84a0be0
rwirba <rwirba@protonmail.com>
2025-09-15 15:16:14 -0500

    added test file

---
+++ src/test/java/com/ezlearn/AppTest.java
@@ -0,0 +1,11 @@
+package com.ezlearn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void sanity() {
+        assertTrue(true);
+    }
+}
